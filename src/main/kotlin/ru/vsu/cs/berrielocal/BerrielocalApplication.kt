27821b3cf2111fa360babf9207763d84dd6fcd12
Максим Долженко <maxim_2003@mail.ru>
2024-03-10 20:42:38 +0300

    BERRIELOCAL-2: init

---
+++ src/main/kotlin/ru/vsu/cs/berrielocal/BerrielocalApplication.kt
@@ -0,0 +1,11 @@
+package ru.vsu.cs.berrielocal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BerrielocalApplication
+
+fun main(args: Array<String>) {
+    runApplication<BerrielocalApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vsu/cs/berrielocal/BerrielocalApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vsu.cs.berrielocal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BerrielocalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
