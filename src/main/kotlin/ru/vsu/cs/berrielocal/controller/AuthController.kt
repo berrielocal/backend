dab4a8bf6ff6aa88d47e73cfacf85fef71f589b9
Максим Долженко <maxim_2003@mail.ru>
2024-04-11 17:27:33 +0300

    BERRIELOCAL-22: shop controller

--- src/main/kotlin/ru/vsu/cs/berrielocal/controller/AuthController.kt
+++ src/main/kotlin/ru/vsu/cs/berrielocal/controller/AuthController.kt
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
